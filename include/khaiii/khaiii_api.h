6d91f27d9dce3f9fe72de5e527eff0ec9513217d
krikit <krikit@naver.com>
2018-12-27 21:02:09 +0900

    version from 0.2 to 0.3

--- include/khaiii/khaiii_api.h
+++ include/khaiii/khaiii_api.h
@@ -12,7 +12,7 @@
 // constants //
 ///////////////
 #define KHAIII_VERSION_MAJOR 0
-#define KHAIII_VERSION_MINOR 2
+#define KHAIII_VERSION_MINOR 3
 #define _MAC2STR(m) #m
 #define _JOIN_VER(x,y) _MAC2STR(x) "." _MAC2STR(y)    // NOLINT
 #define KHAIII_VERSION _JOIN_VER(KHAIII_VERSION_MAJOR,KHAIII_VERSION_MINOR)    // NOLINT
