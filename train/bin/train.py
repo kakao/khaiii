f9d97d3d4bc7c5e5cb0da83acc490837d5a64c0f
Jamie <jamie.lim@kakaocorp.com>
2019-01-23 22:13:31 +0900

    batch grow 파라미터, epoch 개념을 없애고 학습과정을 단순화함 #30

--- train/bin/train.py
+++ train/bin/train.py
@@ -40,36 +40,30 @@ def main():
     main function processes only argument parsing
     """
     parser = ArgumentParser(description='train model from data')
-    parser.add_argument('-i', '--in-pfx', help='input data prefix', metavar='NAME', required=True)
+    parser.add_argument('-i', '--in-pfx', help='input data path prefix', metavar='NAME',
+                        required=True)
     parser.add_argument('--rsc-src', help='train resource dir <default: ../rsc/src>', metavar='DIR',
                         default='../rsc/src')
-    parser.add_argument('--log-dir', help='tensorboard log dir', metavar='DIR',
+    parser.add_argument('--logdir', help='tensorboard log dir <default: ./logdir>', metavar='DIR',
                         default='./logdir')
-    parser.add_argument('--window', help='left/right character window length', metavar='INT',
-                        type=int,
-                        default=3)
-    parser.add_argument('--spc-dropout', help='space(word delimiter) dropout rate', metavar='REAL',
-                        type=float,
-                        default=0.0)
-    parser.add_argument('--cutoff', help='cutoff', metavar='INT', type=int,
-                        default=2)
-    parser.add_argument('--embed-dim', help='embedding dimension', metavar='INT', type=int,
-                        default=30)
-    parser.add_argument('--learning-rate', help='learning rate', metavar='REAL', type=float,
-                        default=0.001)
-    parser.add_argument('--lr-decay', help='learning rate decay', metavar='REAL', type=float,
-                        default=0.9)
-    parser.add_argument('--batch-size', help='batch size', metavar='INT', type=int,
-                        default=500)
-    parser.add_argument('--check-iter', help='check iteration', metavar='INT', type=int,
-                        default=10000)
-    parser.add_argument('--batch-grow', help='batch growing iteration. 0 means no growing',
-                        type=int,
-                        default=10000)
-    parser.add_argument('--patience', help='maximum patience count to revert model', metavar='INT',
-                        type=int,
-                        default=10)
-    parser.add_argument('--gpu-num', help='GPU number to use', metavar='INT', type=int,
+    parser.add_argument('--window', help='left/right character window length <default: 3>',
+                        metavar='INT', type=int, default=3)
+    parser.add_argument('--spc-dropout', help='space(word delimiter) dropout rate <default: 0.0>',
+                        metavar='REAL', type=float, default=0.0)
+    parser.add_argument('--cutoff', help='cutoff <default: 2>', metavar='INT', type=int, default=2)
+    parser.add_argument('--embed-dim', help='embedding dimension <default: 30>', metavar='INT',
+                        type=int, default=30)
+    parser.add_argument('--learning-rate', help='learning rate <default: 0.001>', metavar='REAL',
+                        type=float, default=0.001)
+    parser.add_argument('--lr-decay', help='learning rate decay <default: 0.9>', metavar='REAL',
+                        type=float, default=0.9)
+    parser.add_argument('--batch-size', help='batch size <default: 1000>', metavar='INT', type=int,
+                        default=1000)
+    parser.add_argument('--check-step', help='check every N step <default: 10000>', metavar='INT',
+                        type=int, default=10000)
+    parser.add_argument('--patience', help='maximum patience count to revert model <default: 10>',
+                        metavar='INT', type=int, default=10)
+    parser.add_argument('--gpu-num', help='GPU number to use <default: 0>', metavar='INT', type=int,
                         default=0)
     parser.add_argument('--debug', help='enable debug', action='store_true')
     args = parser.parse_args()
